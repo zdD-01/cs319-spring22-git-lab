3d2a33dc08b0c8bfa1d299cbb6158334a4b023e1
Zeynep Derin Dedeler <derin.dedeler@ug.bilkent.edu.tr>
2022-03-04 11:11:02 +0300

    add evaluation code in main method

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        NewCalculator calculator = new NewCalculator();
 
+        // Try division
+        calculator.Divide(100, 12);
+        System.out.println(calculator.getLastResault());
+
+        // Try addition
+        calculator.Add(10, 35);
+        System.out.println(calculator.getLastResault());
+
+        // Try substraction
+        calculator.Add(50, 15);
+        System.out.println(calculator.getLastResault());
     }
 }
